8c6e60767c94d10c776d8ed3dd32e539d962cd82
surunzi <surunzi@foxmail.com>
2015-12-13 18:05:02 +0800

    Add: Button style

---
+++ eustia/reqAnimFrame.js
@@ -0,0 +1,9 @@
+'now';
+
+var interval = 1000 / 60;
+
+reqAnimFrame = window.webkitRequestAnimationFrame ||
+               window.mozRequestAnimationFrame    ||
+               window.oRequestAnimationFrame      ||
+               window.msRequestAnimationFrame     ||
+               function (cb) { setTimeout(function () { cb(now()) }, interval) };
