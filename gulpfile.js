4818f6284778b5371b354fa78c4e24ed465b0851
surunzi <surunzi@foxmail.com>
2017-03-29 19:41:39 +0800

    Add: Htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
+    htmlmin = require('gulp-htmlmin'),
     uglify = require('gulp-uglify');
 
-gulp.task('build', function ()
+gulp.task('js', function ()
 {
     return gulp.src('dist/static/js/*.js')
                .pipe(uglify())
                .pipe(rename({suffix: '.min'}))
                .pipe(gulp.dest('dist/static/js'));
 });
+
+gulp.task('html', function () 
+{
+    return gulp.src('dist/*.html')
+               .pipe(htmlmin({
+                   collapseWhitespace: true,
+                   minifyJS: true
+               }))
+               .pipe(gulp.dest('dist')); 
+});
+
+gulp.task('default', ['js', 'html']);
 
