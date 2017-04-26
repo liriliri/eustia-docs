843ffbfad1bdd2bb69faaeb3302cc53452de69bb
surunzi <surunzi@foxmail.com>
2017-04-26 15:13:35 +0800

    Add: Module source link

--- script/updateMod.js
+++ script/updateMod.js
@@ -5,7 +5,24 @@ var ERIS_URL = 'https://raw.githubusercontent.com/liriliri/eris/master/doc.md';
 
 request(ERIS_URL, function (err, res, body)
 {
+    if (err) return console.log(err);
+
+    body = addSourceLink(body);
+
     var data = '---\nlayout: module.jade\ntitle: Module\n---\n\n' + body;
 
-    fs.writeFile('src/module.md', data, 'utf-8');
+    fs.writeFile('src/module.md', data, 'utf-8', function (err) 
+    {
+        if (err) console.log(err);
+    });
 });
+
+function addSourceLink(body) 
+{
+    return body.replace(/^##\s+([\w$]+)/mg, function (match, name) 
+    {
+        var source = 'https://github.com/liriliri/eris/blob/master/' + name[0].toLowerCase() + '/' + name + '.js';
+
+        return match + '\n\n[source](' + source + ')'; 
+    });
+}
